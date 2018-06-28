f377b819dbc61666535a5d84673e0225a6e4b215
Sebastian Strus <sebastian.strus1@gmail.com>
2018-06-28 02:16:28 +0200

    first commit

---
+++ Foody2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Foody2
+//
+//  Created by Sebastian Strus on 2018-06-28.
+//  Copyright © 2018 Sebastian Strus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
